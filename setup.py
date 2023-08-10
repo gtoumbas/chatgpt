38687fe8f9eb89acded2d51d9eb25d5b10bbde00
gtoumbas <70962697+gtoumbas@users.noreply.github.com>
2023-08-10 16:26:52 +0100

    setup py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='chatgpt',
     version='0.1',
-    packages=find_packages(),
+    packages=['chatgpt']
     description='A simple package to interact with the OpenAI API',
     author='George Toumbas',
     author_email='gtoumbas23@gmail.com',
-    url='https://github.com/yourusername/your-package-name',
+    licesne='MIT',
+    url='https://github.com/gtoumbas/chatgpt.git',
     install_requires=[
         'openai',
     ],

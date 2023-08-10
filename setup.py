83961b623e5a3b52e91a3320b28f14cfd83d1a4a
gtoumbas <70962697+gtoumbas@users.noreply.github.com>
2023-08-10 16:19:06 +0100

    setup py

--- setup.py
+++ setup.py
@@ -11,12 +11,4 @@ setup(
     install_requires=[
         'openai',
     ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
 )
